ba09299e296f102322af3e9a19afcc5795289308
MPOWELL9 <mckaylaapowell@gmail.com>
2024-09-18 00:14:50 -0400

    first

---
+++ my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Greeter from './Greeter';
+
+
+function App() {
+  return (
+    <div>
+    <h1>Welcome to My React App</h1>
+     <Greeter /> {/* Use the Greeter component*/}
+      </div>
+  );
+}
+export default App;
